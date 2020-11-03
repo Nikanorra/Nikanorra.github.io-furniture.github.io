571fc07957bc02ad7900de52b38909cd76c7bbad
Nikanora <37082879+Nikanorra@users.noreply.github.com>
2020-11-03 10:22:05 +0200

    Add files via upload

--- js/script-slick.js
+++ js/script-slick.js
@@ -3,11 +3,12 @@
 		  infinite: true,
 		  speed: 1000,
 			cssEase: 'linear',
-			autoplaySpeed: 9000,
+			autoplaySpeed: 100000,
 		  fade: true,
 		  cssEase: 'linear',
 			autoplay: true,
 			mobileFirst: true,
-			// appendArrows: $('.propose__text-dots-left, .propose__text-dots-right')
+			prevArrow: '.propose__text-dots-left',
+			nextArrow: '.propose__text-dots-right'
 		});
 	});
